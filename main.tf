dea618a71b7ce9f501a058b42eec0b10a8c2040c
Hemant Kumar <38708271+HemantTomar@users.noreply.github.com>
2023-04-13 00:04:47 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  region = "ap-south-1" # Replace with your desired AWS region
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c95c71c99fe1e7c3" # Replace with your desired EC2 instance AMI ID
+  instance_type = "t2.micro" # Replace with your desired EC2 instance type
+
+  tags = {
+    Name = "example-instance"
+  }
+
+  # Specify the security group for the instance
+  security_group = aws_security_group.example.id
+}
+
+resource "aws_security_group" "example" {
+  name_prefix = "example"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Replace with your desired source IP range for SSH access
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
