be8d315dbcc46973f948576276ef6b305730c2fa
Hemant Kumar <38708271+HemantTomar@users.noreply.github.com>
2023-04-13 09:09:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = "ap-south-1" 
 }
 
+terraform {
+  backend "s3" {
+    bucket = "<your-s3-bucket-name>"
+    key    = "<your-state-file-key>"
+    region = "<your-s3-region>"
+  }
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-07d3a50bd29811cd1" 
   instance_type = "t2.micro" 
